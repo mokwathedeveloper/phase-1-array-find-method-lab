832076cd62570b09a5a458ed089837e9f247b2f7
MokwaTheDeveloper <moffatmokwa12@gmail.com>
2023-09-30 18:16:28 +0300

    completed Assigment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // code your solution here
+function superbowlWin(record) {
+    const winningYear = record.find(obj => obj.result === "W");
+    return winningYear ? winningYear.year : undefined;
+  }
